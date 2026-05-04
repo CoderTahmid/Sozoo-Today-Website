95b5308006930da5ed5e412363739c78dfde49f2
Tahmid Ibne Mofazzol <tahmid.ibne@gmail.com>
2026-05-05 02:12:45 +0600

    success section done

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,15 @@
 import Hero from "../components/Hero";
 import OurPortfolio from "../components/OurPortfolio";
+import SuccessCount from "../components/SuccessCount";
 
 const HomePage = () => {
     return (
         <>
             <Hero></Hero>
             <OurPortfolio></OurPortfolio>
+            <SuccessCount></SuccessCount>
         </>
     );
 };
 
-export default HomePage;
+export default HomePage;    
