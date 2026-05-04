4fd040d7e5375f7c58ee5bdb3aef1697c042d9ae
Tahmid Ibne Mofazzol <tahmid.ibne@gmail.com>
2026-05-04 14:10:27 +0600

    navbar done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,85 @@
 const Navbar = () => {
     return (
-        <div className="navbar p-4">
-            Navbar
-        </div>
-    );
+		<div className="navbar bg-base-100 shadow-sm">
+			{/* Mobile Layout */}
+			<div className="lg:hidden flex justify-between w-full">
+				<a className="btn btn-ghost text-xl">Sozoo Media</a>
+				<div className="dropdown dropdown-end">
+					<div tabIndex={0} role="button" className="btn btn-ghost">
+						<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+							{" "}
+							<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />{" "}
+						</svg>
+					</div>
+					<ul tabIndex="-1" className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+						<li>
+							<a>Home</a>
+						</li>
+						<li>
+							<a>About</a>
+						</li>
+						<li>
+							<a>Sozoo Today</a>
+						</li>
+						<li>
+							<a>Sozoo Entertainment</a>
+						</li>
+						<li>
+							<a>Get The Fame</a>
+						</li>
+						<li>
+							<a>Best Bangaldeshi Ads</a>
+						</li>
+						<li>
+							<a>Services</a>
+						</li>
+						<li>
+							<a>Contact</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+
+			{/* Desktop Layout */}
+			<div className="hidden lg:flex w-full">
+				<div className="navbar-start flex-1">
+					<ul className="menu menu-horizontal px-1">
+						<li>
+							<a>Home</a>
+						</li>
+						<li>
+							<a>About</a>
+						</li>
+						<li>
+							<a>Sozoo Today</a>
+						</li>
+						<li>
+							<a>Sozoo Entertainment</a>
+						</li>
+					</ul>
+				</div>
+				<div className="navbar-center">
+					<a className="btn btn-ghost text-xl">Sozoo Media</a>
+				</div>
+				<div className="navbar-end flex-1">
+					<ul className="menu menu-horizontal px-1">
+						<li>
+							<a>Get The Fame</a>
+						</li>
+						<li>
+							<a>Best Bangaldeshi Ads</a>
+						</li>
+						<li>
+							<a>Services</a>
+						</li>
+						<li>
+							<a>Contact</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Navbar;
